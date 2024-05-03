f320c598b5193c0612aef1ae0e0c9869842dc831
SindhuK1234 <167416438+SindhuK1234@users.noreply.github.com>
2024-05-03 19:38:45 +0530

    Add files via upload

---
+++ examp3.py
@@ -0,0 +1,21 @@
+'''million=1_000_000
+binary=0b_1110
+octa=0o_64
+hexa=0x_23_ab
+print(million)
+print(binary)
+print(octa)
+print(hexa)'''
+
+
+'''na=['sindhu','divya','pavi','poorvi','pragna']
+for i in range(len(na)):
+    print(i)
+for i in range(len(na)):
+    print(na[i])
+for i in range(len(na)):
+    print(na[i],'B.TECH')'''
+
+
+a,*_,b=(7,6,5,4,3,2,1)#omitting values *variable is used to assign multiple values
+print(a,b)
--- first.py
+++ first.py
@@ -1,8 +1,1 @@
-num1=int(input("Enter the num1:"))
-num2=int(input("Enter the num2:"))
-if(num1>num2):
-    print(num1,'num1 is greater')
-elif(num1==num2):
-    print('both r equal')
-else:
-    print(num2,'num2 is greater')
+print("Hello world")
